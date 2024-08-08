0bb715fa53b0b511c2064d215cfc956257affb41
jayalakshmi0308 <jayalakshmi456@revature.com>
2024-08-08 04:36:33 -0700

    second commit

--- app/Controllers/menuCtrl.js
+++ app/Controllers/menuCtrl.js
@@ -1,11 +1,12 @@
 
 
 
-var app = angular.module('app');
 app.controller('menuCtrl', ['$scope', '$http', function($scope, $http) {
     var apiKey = '18ac5e05ee504848a6736921760327f9';
-     var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey;
-    
+    var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey;
+
+    $scope.cart = JSON.parse(localStorage.getItem('cart')) || [];
+    $scope.notification = '';
 
     $http.get(apiUrl)
     .then(function(response){
@@ -26,32 +27,57 @@ app.controller('menuCtrl', ['$scope', '$http', function($scope, $http) {
             console.log("Error fetching salad details", error);
         });
     };
-}]);
-$scope.addToCart = function() {
-    if ($scope.selectedSalad) {
-        
-        var itemExists = $scope.cart.some(item => item.id === $scope.selectedSalad.id);
 
-        if (!itemExists) {
-            $scope.cart.push($scope.selectedSalad);
-            $scope.selectedSalad = null;
-            alert("Added to cart!");
-        } else {
-            alert("Item is already in the cart!");
+    $scope.addToCart = function() {
+        if ($scope.selectedSalad) {
+            var itemExists = $scope.cart.some(item => item.id === $scope.selectedSalad.id);
+
+            if (!itemExists) {
+                $scope.cart.push($scope.selectedSalad);
+                localStorage.setItem('cart', JSON.stringify($scope.cart));
+                $scope.notification = "Your food added to cart!";
+                setTimeout(function() {
+                    $scope.$apply(function() {
+                        $scope.notification = '';
+                    });
+                }, 3000);
+                $scope.selectedSalad = null;
+            } else {
+                alert("Item is already in the cart!");
+            }
         }
-    }
-};
+    };
+}]);
 
-$scope.orderCart = function() {
-    if ($scope.cart.length > 0) {
-      
-        alert("Order is ready and will be delivered in 20 minutes!");
-        $scope.cart = []; 
-    } else {
-        alert("Your cart is empty!");
-    }
-};
+app.controller('cartCtrl', ['$scope', function($scope) {
+    $scope.cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+    $scope.orderCart = function() {
+        if ($scope.cart.length > 0) {
+            alert("Order is ready and will be delivered in 20 minutes!");
+            $scope.cart = [];
+            localStorage.removeItem('cart');
+        } else {
+            alert("Your cart is empty!");
+        }
+    };
+}]);
+app.controller('cartCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.cart = JSON.parse(localStorage.getItem('cart')) || [];
+    $scope.orderPlaced = false;
 
+    $scope.orderCart = function() {
+        if ($scope.cart.length > 0) {
+            $scope.orderPlaced = true;
+            $scope.cart = [];
+            localStorage.removeItem('cart');
 
+            $timeout(function() {
+                $scope.orderPlaced = false;
+            }, 5000);
+        } else {
+            alert("Your cart is empty!");
+        }
+    };
+}]);
 
